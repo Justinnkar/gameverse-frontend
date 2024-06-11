1168d14aaf286b38e51c39fd02d9478eafb70774
Justinnkar <jkarpman01@gmail.com>
2024-06-11 13:18:55 -0700

    removed unused href

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,34 +1,27 @@
-import React from "react"
-import { Navbar, Button } from "reactstrap"
-import "../styles/Footer.css"
-import Ylogo from "../assets/Ylogo.png"
-import Top from "../assets/Top.png"
+import React from "react";
+import { Navbar, Button } from "reactstrap";
+import "../styles/Footer.css";
+import Ylogo from "../assets/Ylogo.png";
+import Top from "../assets/Top.png";
 
 const Footer = () => {
-
   return (
     <>
-     <Navbar className="foot-navbar" color="transparent">
+      <Navbar className="foot-navbar" color="transparent">
         <div>
           <img src={Ylogo} alt="Ylogo png" className="yfooter-logo" />
         </div>
         <div>
-          <a  href="#">
           <img src={Top} alt="top png" className="top-btn" />
-          </a>
         </div>
-        <div className='btn2-container'>
-          <Button
-            className='aboutus-view-btn'
-            size="sm"
-            href="/aboutus"
-          >
+        <div className="btn2-container">
+          <Button className="aboutus-view-btn" size="sm" href="/aboutus">
             About Us
           </Button>
         </div>
       </Navbar>
     </>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
